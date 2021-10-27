e4a268aceb47628c9bcf4dcb046970d6047b47e8
Larry Sui <larry.sui@vidizmo.com>
2021-10-26 22:32:20 -0400

    Add webapp_appinsight

--- arm/build.ps1
+++ arm/build.ps1
@@ -7,8 +7,8 @@ New-AzResourceGroup -Name $rg -Location eastus -Force
 New-AzResourceGroupDeployment `
     -Name 'Winimage' `
     -ResourceGroupName $rg `
-    -TemplateFile 'json\web\1appplan_webapp.json' `
-    -TemplateParameterFile '.\json\web\1appplan_webapp.parameters.json' `
+    -TemplateFile 'json\web\3webapp_appinsight.json' `
+    -TemplateParameterFile '.\json\web\3webapp_appinsight.parameters.json' `
     -Verbose
     
 
